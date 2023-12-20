66824de30cd0812356c399475942259a48078423
Naseef <naseef.abdullah@gainsco.com>
2023-12-19 22:18:26 -0600

    deleted old porject

--- Gainsco.ClaimCenter.CodedUITests/RunDeployedTests.ps1
+++
@@ -1,7 +0,0 @@
-﻿$mainDirectory = $(Get-Item $($MyInvocation.MyCommand.Path)).DirectoryName
-$NUnitConsoleExe = "$mainDirectory\nunit3-console.exe"
-$ReportUnitExe = "$mainDirectory\ReportUnit.exe"
-
-&$NUnitConsoleExe Gainsco.ClaimCenter.CodedUITests.dll
-
-&$ReportUnitExe "TestResult.xml"
